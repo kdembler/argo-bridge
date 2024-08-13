8ddef6f12338ab5d8657fd47473eb134ae69a365
Klaudiusz Dembler <accounts@kdembler.com>
2024-08-13 19:47:35 +0200

    testnet adjustments

--- packages/app/src/config.ts
+++ packages/app/src/config.ts
@@ -3,7 +3,7 @@ import { EVM_NETWORKS, JOY_NETWORKS } from '@joystream/argo-core'
 // export const RPC_URL = 'wss://rpc.joyutils.org'
 // export const ARGO_INDEXER_URL = 'http://localhost:4350/graphql'
 // export const ARGO_INDEXER_URL = 'https://176.9.77.179.nip.io/argo/graphql'
-export const ARGO_INDEXER_URL = 'https://argo.joyutils.org/graphql'
+export const ARGO_INDEXER_URL = 'https://argo-testnet.joyutils.org/graphql'
 
 export const JOYSTREAM_CHAIN_ID = 'polkadot:6b5e488e0fa8f9821110d5c13f4c468a'
 export const JOYSTREAM_SS58_PREFIX = 126
@@ -20,8 +20,8 @@ export const WC_METADATA = {
 export const EVM_OUT_OF_SYNC_THRESHOLD = 9
 export const JOY_OUT_OF_SYNC_THRESHOLD = 3
 
-export const EVM_NETWORK = EVM_NETWORKS.base
-export const JOY_NETWORK = JOY_NETWORKS.mainnet
+export const EVM_NETWORK = EVM_NETWORKS.baseSepolia
+export const JOY_NETWORK = JOY_NETWORKS.petra
 export const BRIDGE_ADDRESS = EVM_NETWORK.contracts.bridge
 export const TIMELOCK_ADDRESS = EVM_NETWORK.contracts.timelock
 export const ERC20_ADDRESS = EVM_NETWORK.contracts.erc20
